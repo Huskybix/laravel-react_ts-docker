a0059e0b608efda5a1495439e7872f5cd683de17
N Storie <huskybix@outlook.com>
2026-04-12 22:21:32 +0100

    Attempt at fixing validator in prod

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -16,6 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->validateCsrfTokens(except: [
+            'products/validate',
+        ]);
+
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckUserRole::class,
         ]);
