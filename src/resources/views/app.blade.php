aa2f590db03d4e3480162729cf0cc962a21cd703
N Storie <huskybix@outlook.com>
2026-04-20 14:39:46 +0100

    Added favicon

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Niall Storie - Huskybix</title>
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/husky-icon-orange.png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
